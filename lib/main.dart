bcb2ff0c1720f3ac617223156eda672f2a36536b
rlv28 <rebeccalhawthorne@gmail.com>
2025-05-04 12:39:26 -0400

    added favaorites page functionality

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,34 @@ class MyAppState extends ChangeNotifier {
 
 }
 
+class FavoritesPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty){
+      return Center(
+        child: Text("No Favorites yet.") ,
+        );
+    }
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+          '${appState.favorites.length} favorites:'),
+          ),
+          for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
+
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -60,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
